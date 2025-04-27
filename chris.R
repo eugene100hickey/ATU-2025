4af5f94738729f8c7d66565b29cda5c29be5731f
eugene100hickey <eugene.hickey@tudublin.ie>
2025-04-27 20:46:13 +0100

    major project out

---
+++ chris.R
@@ -0,0 +1,21 @@
+library(tidyverse)
+
+z_file <- "../ATU-2025-private/data/chris/s1m10339-chris.txt"
+
+z <- read.delim(z_file) |> 
+  janitor::clean_names()
+
+indices <- which(z$comment == "MM")
+
+z1 <- z[indices[1]:indices[2],] |> 
+  as_tibble() |> 
+  filter(value != "MM")
+
+z2 <- z$comment |> as.numeric()
+z3 <- z[is.na(z2),]
+z4 <- z[!is.na(z2),] |> as.numeric()
+
+
+zq <- tibble(x = z$comment[141:20141] |> as.numeric(), y =  z$comment[20146:40146] |> as.numeric())
+
+zq |> ggplot(aes(x, y)) + geom_point()
